99400ef0abbe028c97129bb848cb77831f22ad42
OlgaMatsiuk <matsiuk2015@gmail.com>
2024-06-24 20:56:07 +0300

    Update TaskListTest.java

--- src/test/java/TaskListTest.java
+++ src/test/java/TaskListTest.java
@@ -16,6 +16,7 @@ public class TaskListTest extends TestBase{
         app.getUser().click(By.xpath("//button[text()=' Add task ']"));
         app.getUser().click(By.xpath("//button[@class='round-btn btn-icon-behavior align-self-start']//i[@class='mdi mdi-pencil']"));
         //app.getUser().type(By.xpath("//div[@class='task-name font-weight-500']"), "First task");
+        app.getUser().type(By.xpath("//input[@class='form-control form-control-xsm w75 ng-untouched ng-pristine ng-valid']"),"First task");
 
         app.getUser().type(By.xpath("//textarea[@id='textarea-input']"),"I want to add my new task");
         app.getTaskList().search("aws");
